5b2ee79918b1eb26066ef5ab2ceecf30b853babc
Dane Sethi <gurdane2000@yahoo.com>
2016-04-07 22:42:56 -0400

    Moderator Controls functionality complete.

--- app/src/main/java/com/sethi/gurdane/usfhvz/ModeratorControls.java
+++ app/src/main/java/com/sethi/gurdane/usfhvz/ModeratorControls.java
@@ -19,9 +19,14 @@ import java.util.Calendar;
 
 public class ModeratorControls extends AppCompatActivity {
 
+    String killId="";
+
     EditText etTitle;
     EditText etBody;
+    EditText etKillId;
     Button btPostAnnouncement;
+    Button btKillHuman;
+    Button btCureZombie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +35,65 @@ public class ModeratorControls extends AppCompatActivity {
 
         etTitle = (EditText)findViewById(R.id.title_text);
         etBody = (EditText)findViewById(R.id.body_text);
+        etKillId = (EditText)findViewById(R.id.kill_id_text);
         btPostAnnouncement = (Button)findViewById(R.id.announcement_button);
+        btKillHuman = (Button)findViewById(R.id.bt_kill_human);
+        btCureZombie = (Button)findViewById(R.id.bt_cure_zombie);
 
         etTitle.setText("");
         etBody.setText("");
+        etKillId.setText("");
 
         btPostAnnouncement.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v) {
+            public void onClick(View v) {
                 //perform action
-                PostAnnouncement obj = new PostAnnouncement();
-                obj.execute();
+                //Do not post if title is blank
+                if (etTitle.getText().toString().trim().equals("")) {
+                    Toast.makeText(ModeratorControls.this, "No title entered for announcement!", Toast.LENGTH_SHORT).show();
+                    //Do not post if body is blank
+                } else if (etBody.getText().toString().trim().equals("")) {
+                    Toast.makeText(ModeratorControls.this, "No body entered for announcement!", Toast.LENGTH_SHORT).show();
+                } else { //Post announcement
+                    PostAnnouncement obj = new PostAnnouncement();
+                    obj.execute();
+                }
+            }
+        });
+
+        btKillHuman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //perform action
+                //Do not post if KillId is blank
+                if (etKillId.getText().toString().trim().equals("")) {
+                    Toast.makeText(ModeratorControls.this, "No kill ID entered!", Toast.LENGTH_SHORT).show();
+                    //Do not post if KillId is invalid
+                } else if (etKillId.getText().toString().trim().length() != 6) {
+                    Toast.makeText(ModeratorControls.this, "Invalid kill ID!", Toast.LENGTH_SHORT).show();
+                } else { //Kill human
+                    killId = etKillId.getText().toString();
+                    KillHuman obj = new KillHuman();
+                    obj.execute();
+                }
+            }
+        });
+
+        btCureZombie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //perform action
+                //Do not post if KillId is blank
+                if (etKillId.getText().toString().trim().equals("")) {
+                    Toast.makeText(ModeratorControls.this, "No kill ID entered!", Toast.LENGTH_SHORT).show();
+                    //Do not post if KillId is invalid
+                } else if (etKillId.getText().toString().trim().length() != 6) {
+                    Toast.makeText(ModeratorControls.this, "Invalid kill ID!", Toast.LENGTH_SHORT).show();
+                } else { //Cure zombie
+                    killId = etKillId.getText().toString();
+                    CureZombie obj = new CureZombie();
+                    obj.execute();
+                }
             }
         });
     }
@@ -83,6 +136,52 @@ public class ModeratorControls extends AppCompatActivity {
             etBody.setText("");
             Intent intent = new Intent (getApplicationContext(), Home.class);
             startActivity(intent);
+        }
+    }
+
+    public class KillHuman extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String...params) {
+            //try {
+                //Initialize DynamoDB client and object mapper
+                AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(Home.credentialsProvider);
+                DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+                USFHvZ_Users user = mapper.load(USFHvZ_Users.class,killId);
+                user.setState("Zombie");
+                mapper.save(user);
+            //} catch (Exception e) {
+                //handle exception
+            //}
+            return null;
+        }
+
+        protected void onPostExecute(String page) {
+            //onPostExecute
+            etKillId.setText("");
+        }
+    }
+
+    public class CureZombie extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String...params) {
+            try {
+                //Initialize DynamoDB client and object mapper
+                AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(Home.credentialsProvider);
+                DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+                USFHvZ_Users user = mapper.load(USFHvZ_Users.class,killId);
+                user.setState("Human");
+                mapper.save(user);
+            } catch (Exception e) {
+                //handle exception
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String page) {
+            //onPostExecute
+            etKillId.setText("");
         }
     }
 }
